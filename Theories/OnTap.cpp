a85358d1ce28a479908fd99cf050fd87c5cb2ff8
tun784 <thudientu782004@gmail.com>
2023-12-26 07:16:56 +0700

    26-12-2023 07:16

--- Theories/OnTap.cpp
+++ Theories/OnTap.cpp
@@ -5,13 +5,21 @@
 #include <string.h>
 #include <time.h>
 using namespace std;
-int GiaiThua(int n){
-    return n < 1 ? 1 : n * GiaiThua(n - 1);
+int factorial(int n){
+    return n < 1 ? 1 : n * factorial(n - 1);
+}
+int fibonacci(int n){
+    return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+}
+int nCk(int n, int k){
+    return (n == k || k == 0) ? 1 : factorial(n) / (factorial(k) * factorial(n - k));
 }
 int main(){
-    int so;
-    cout << "Nhap so bat ky: ";
-    cin >> so;
-    cout << "Ket qua giai thua cua " << so << " la: " << GiaiThua(so) << endl;
+    int a, b;
+    cout << "Nhap so a va so b bat ky: ";
+    cin >> a >> b;
+    cout << "Ket qua giai thua cua " << a << " la: " << factorial(a) << endl;
+    cout << "So Fibonacci cua " << a << " la: " << fibonacci(a) << endl;
+    cout << "Ket qua cua " << a << " chap " << b << " la: " << nCk(a, b);
     return 0;
 }
