dd711e2cabcb2073e7008f9ee13a18449ea80355
tun784 <thudientu782004@gmail.com>
2023-12-26 10:56:22 +0700

    26-12-2023 10:56

--- Theories/so_nguyen_lon_N.c
+++ Theories/so_nguyen_lon_N.c
@@ -3,54 +3,46 @@
 
 int main()
 {
-    long N;
-    int a, c;
-    int tong = 0;
-    int max = 0;
+    long N, n, a, c;
+    int tong = 0, max = 0;
 
     printf("Nhap gia tri N: ");
-    scanf("%ld", &N);  // Sửa lỗi: Sử dụng %ld để đọc long
-
+    scanf("%ld", &N);
+    n = N;
     // Tìm chữ số hàng đầu tiên
-    a = N;  // Sửa lỗi: gán a = N để lấy chữ số hàng đầu tiên
-    while (a >= 10)
-    {
+    a = N;  // Gán a = N để lấy chữ số hàng đầu tiên
+    while (a >= 10) {
         a = a / 10;
     }
 
     // Tính tổng các chữ số
-    while (N != 0)
-    {
+    while (N != 0){
         c = N % 10;
         tong += c;
         N /= 10;
     }
 
     // Tìm chữ số lớn nhất
-    N = N;  // Sửa lỗi: Reset N để tìm chữ số lớn nhất
-    while (N != 0)
-    {
+    N = n;  // Reset N để tìm chữ số lớn nhất
+    while (N != 0){
         c = N % 10;
         if (c > max)
-        {
             max = c;
-        }
         N /= 10;
     }
 
     // Đếm số chữ số của N
-    int scs = 0;
-    N = N;  // Sửa lỗi: Reset N để đếm số chữ số
-    while (N != 0)
-    {
-        scs++;
+    int soChuSo = 0;
+    N = n;  // Reset N để đếm số lượng chữ số
+    while (N != 0) {
+        soChuSo++;
         N /= 10;
     }
 
     printf("\nChu so hang dau tien cua N la: %d", a);
     printf("\nTong cac chu so cua N la: %d", tong);
     printf("\nChu so lon nhat la: %d", max);
-    printf("\nSo chu so la: %d", scs);
+    printf("\nSo chu so la: %d", soChuSo);
 
     return 0;
 }
